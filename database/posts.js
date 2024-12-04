94dce6f67a100bd15351d9168155de262ca611c6
carolina <ci.arayagonzalez@gmail.com>
2024-12-04 03:14:39 -0300

    add path to modify a publication

--- database/posts.js
+++ database/posts.js
@@ -1,23 +1,44 @@
-import { conexion } from "./conexion.js"
+import { conexion } from "./conexion.js";
 
 const getPosts = async (userId) => {
-    const response = await conexion.query("SELECT * FROM posts WHERE user_id=$1", [userId])
-    return response.rows
-}
+  const response = await conexion.query(
+    "SELECT * FROM posts WHERE user_id=$1",
+    [userId]
+  );
+  return response.rows;
+};
 
-const setPost = async ( userId, postData ) => {
+const setPost = async (userId, postData) => {
+  const argumentos = {
+    text: "INSERT INTO posts(user_id, title, content) VALUES($1, $2, $3) RETURNING * ;",
+    values: [userId, postData.title, postData.content],
+  };
+
+  const post = await conexion.query(argumentos);
+  if (post.rowCount == 0) {
+    return null;
+  }
+  const postPublished = post.rows[0];
+
+  return postPublished;
+};
+
+const updatePost = async (postData, postId, userId) => {
+  try {
     const argumentos = {
-        text: "INSERT INTO posts(user_id, title, content) VALUES($1, $2, $3) RETURNING * ;",
-        values: [userId, postData.title, postData.content]
+      text: "UPDATE posts SET title=$1, content=$2 WHERE id=$3 AND user_id=$4 RETURNING * ",
+      values: [postData.title, postData.content, postId.id, userId],
+    };
+    const response = await conexion.query(argumentos);
+    console.log(postId);
+    if (response.rowCount == 0) {
+      return null;
     }
-    
-    const post = await conexion.query(argumentos)
-    if(post.rowCount == 0){
-        return null
-    }
-    const postPublished = post.rows[0]
+    const postUpdated = response.rows[0];
+    return postUpdated;
+  } catch (err) {
+    console.log(err);
+  }
+};
 
-    return postPublished
-}
-
-export { getPosts, setPost }
+export { getPosts, setPost, updatePost };
--- index.js
+++ index.js
@@ -1,48 +1,73 @@
-import express, { response } from "express"
-import jwt from "jsonwebtoken"
-import { getPosts, setPost } from "./database/posts.js"
-import { auth } from "./database/users.js"
+import express, { response } from "express";
+import jwt from "jsonwebtoken";
+import { getPosts, setPost, updatePost } from "./database/posts.js";
+import { auth } from "./database/users.js";
 
+const app = express();
+const port = 3000;
+const secretKey = "um0987";
 
-const app = express()
-const port = 3000
-const secretKey = "um0987"
-
-app.use(express.json())
+app.use(express.json());
 
 app.post("/login", async (req, res) => {
-    const { username, password } = req.body
-    const user = await auth(username, password)
-    if(!user){
-        return response.status(401).json({ message: "Autentificación fallida." })
-    }
-    const token = jwt.sign(user, secretKey)
-    res.json({ message: "Registro exitoso", token: token})
-})
+  const { username, password } = req.body;
+  const user = await auth(username, password);
+  if (!user) {
+    return response.status(401).json({ message: "Autentificación fallida." });
+  }
+  const token = jwt.sign(user, secretKey);
+  res.json({ message: "Registro exitoso", token: token });
+});
 
 app.get("/posts", async (req, res) => {
+  const isAuth = req.headers.authorization;
+  if (!isAuth) {
+    return res
+      .status(401)
+      .json({ message: "Usuario no autorizado, debe enviar token de acceso." });
+  }
+  const decodedToken = jwt.verify(isAuth, secretKey);
+  const posts = await getPosts(decodedToken.id);
 
-    const isAuth = req.headers.authorization
-    if(!isAuth){
-        return res.status(401).json({ message: "Usuario no autorizado, debe enviar token de acceso." })
-    }
-    const decodedToken = jwt.verify(isAuth, secretKey)
-    const posts = await getPosts(decodedToken.id)
-
-    res.json({ message: "Listar posts", data: posts })
-})
+  res.json({ message: "Listar posts", data: posts });
+});
 
 app.post("/posts", async (req, res) => {
-    const postData = req.body
-    const codedToken = req.headers.authorization
-    if(!codedToken){
-        return res.status(401).json({ message: "Usuario no autorizado para publicar. Debe enviar token de acceso." })
-    }
-    const decoded = jwt.verify(codedToken, secretKey)
-    const newPost = await setPost(decoded.id, postData)
-    res.json({ message: "Post registrado con exito", data: newPost })
-})
+  const postData = req.body;
+  const codedToken = req.headers.authorization;
+  if (!codedToken) {
+    return res
+      .status(401)
+      .json({
+        message:
+          "Usuario no autorizado para publicar. Debe enviar token de acceso.",
+      });
+  }
+  const decoded = jwt.verify(codedToken, secretKey);
+  const newPost = await setPost(decoded.id, postData);
+  res.json({ message: "Publicación registrada con exito", data: newPost });
+});
+
+app.put("/posts", async (req, res) => {
+  const dataPost = req.body;
+  const idPost = req.query;
+  const token = req.headers.authorization;
+
+  if (!token) {
+    return res
+      .status(401)
+      .json({
+        message:
+          "Usuario no autorizado para modificar publicación. Debe enviar token de acceso.",
+      });
+  }
+  const decoded = jwt.verify(token, secretKey);
+  const userId = decoded.id;
+
+  const postUpdated = await updatePost(dataPost, idPost, userId);
+  res.json({ message: "Publicación actualizada exitosamente", data: postUpdated });
+});
 
 app.listen(port, () => {
-    console.log(`Aplicación ejecutandose en el puerto ${port}`)
-})
+  console.log(`Aplicación ejecutandose en el puerto ${port}`);
+});
